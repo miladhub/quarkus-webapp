71feeb3bcc62705d352d8fc6c4a19f3e4bb63bcc
milad.bourhani <milad.bourhani@accenture.com>
2023-06-23 17:56:50 +0200

    test

--- backend/src/test/java/org/acme/GreetingResourceTest.java
+++ backend/src/test/java/org/acme/GreetingResourceTest.java
@@ -4,18 +4,16 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
-
     @Test
-    public void testHelloEndpoint() {
+    void testHelloEndpoint() {
         given().auth().basic("test", "test")
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(startsWith("Hello, you are test, with groups bar, foo"));
+             .body(is("Hello, you are test, with groups bar, foo"));
     }
-
 }
